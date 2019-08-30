bbbff09f3c2375ba00bb28cc1ad720abc531a3b6
王水胜 <602466011@qq.com>
2019-08-30 11:01:35 +0800

    ini

---
+++ src/views/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+      <button @click="test">加密</button>
+  </div>
+</template>
+
+<script>
+import CryptoJS from 'crypto-js'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    test() {
+        // 加密处理 
+        var password = 'admin'
+        var text = CryptoJS.AES.encrypt(password, 'secret key 123').toString();//使用CryptoJS方法加密
+        console.log(text)
+        // 解密处理 
+        var bytes = CryptoJS.AES.decrypt(text,'secret key 123').toString(CryptoJS.enc.Utf8)
+        console.log(bytes)
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+<style scoped>
+</style>
---
+++ src/views/index2.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    原数据：
+    <br />
+    <el-input v-model="d1" placeholder="请输入内容"></el-input>
+    <el-button type="primary" @click="jiami" plain>加密</el-button>
+    <el-button type="primary" @click="jiemi" plain>解密</el-button>
+    <br />加密数据：
+    <br />
+    <el-input v-model="d2" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+import { Decrypt, Encrypt } from '@/utils/index.js'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      d1: '',
+      d2: ''
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    jiami() {
+      console.log('加密-----', this.d1)
+      let dd = Encrypt(this.d1)
+      console.log(dd)
+      this.d2 = dd
+      // 1 L9Pu/IxfCbYuyYVsp7RjBg==
+      // 2 je3i5/PNczm6BBetLpjVAQ==
+    },
+    jiemi() {
+      console.log('解密-----', this.d2)
+      this.d1 = Decrypt(this.d2)
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+<style scoped>
+</style>
